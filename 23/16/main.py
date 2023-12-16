930fcf9ca629dfc1f068cf0f109f520316bb5ae6
martin Høva <mrhova46@gmail.com>
2023-12-16 10:40:02 +0100

    23 16

--- 23/16/main.py
+++ 23/16/main.py
@@ -7,22 +7,26 @@ class Solution():
         self.data = eval(open(self.filename).read().rstrip())
         
     def solve(self):
-        firstVal = 1
-        while True:
-            coins = [firstVal]
-            for i in range(1, len(self.data)):
-                nextVal = self.findNextVal(self, i, coins)
-                if nextVal == -1:
-                    firstVal += 1
-                    continue
-                coins.append(nextVal)
-            break
-        
+        coins = [1] * len(self.data)
+        changed = True
+        while changed:
+            changed = False
+            for i in range(len(coins)):
+                if not self.isValid(coins, i):
+                    coins[i] += 1
+                    changed = True
         return sum(coins)
-    
-    def findNextVal(self, i, coins):
-        pass
-    
+            
+    def isValid(self, coins, i):
+        for j in range(-2, 3):
+            if i + j < 0 or i + j >= len(coins) or j == 0:
+                continue
+            index = i + j
+            if self.data[index] < self.data[i] and coins[index] >= coins[i]:
+                return False
+            elif self.data[index] == self.data[i] and coins[index] > coins[i]:
+                return False
+        return True
     
     
 def main():
@@ -32,9 +36,9 @@ def main():
     print("--TEST--")
     print(f"Result: {s.solve()}")
     
-    # s = Solution()
-    # print("--MAIN--")
-    # print(f"Result: {s.solve()}")
+    s = Solution()
+    print("--MAIN--")
+    print(f"Result: {s.solve()}")
     
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
     
