9b1c9fbe246ed6a208bfad9097366b33aa6e8a78
Martin Høva <mrhova46@gmail.com>
2025-12-09 10:34:38 +0100

    mend

--- 25/09/main.py
+++ 25/09/main.py
@@ -2,15 +2,14 @@ data = open("input.txt").read().rstrip()
 
 vocals = "aeiouyæøå"
 consonants = "bcdfghjklmnpqrstvwxz"
-print(len(vocals) + len(consonants))
 beans = ""
 for i, c in enumerate(data):
     if c.isnumeric():
         continue
-    
+
     if c in consonants and not (data[i - 2].isnumeric() and data[i + 2].isnumeric()):
         continue
-        
+
     beans += c
 
 print(beans)
